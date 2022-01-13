b1df312556319eebf9b804ddf3b2c58638291de5
Karthag <kd.domanski@gmail.com>
2022-01-13 19:17:46 +0100

    Added regain memory timeline animation.

---
+++ Wolborska/Assets/Scripts/Visibility.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visibility : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        meshRenderer.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //meshRenderer.enabled = false;
+    }
+}
